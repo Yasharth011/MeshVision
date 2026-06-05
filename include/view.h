7794085b746db783ab00577f3b2a794d0b92c60e
Yasharth011 <yasharthgupta10@gmail.com>
2026-06-05 18:02:33 +0530

    view: buttons to request video stream of nodes

--- include/view.h
+++ include/view.h
@@ -1,15 +1,18 @@
 #pragma once
+#include <glib.h>
 #include <gst/gst.h>
 #include <gtk/gtk.h>
-#include <glib.h>
 
 typedef struct _GtkData {
   GtkWidget *sink_widget;
+  GtkWidget *controls_widget;
   GstElement *pipeline;
   GMainLoop *bus_loop;
-  gint64 duration;
+  const gchar *local_ip;
 } GtkData;
 
 GstElement *init_gtksink(GtkData *data);
 
-void create_ui(GtkData* data);
+void create_ui(GtkData *data);
+
+gboolean refresh_ui(GtkData *data);
--- src/view.c
+++ src/view.c
@@ -1,3 +1,6 @@
+#include <batman.h>
+#include <gio/gio.h>
+#include <glib-object.h>
 #include <glib.h>
 #include <glibconfig.h>
 #include <gtk/gtk.h>
@@ -40,16 +43,80 @@ static void delete_event_cb(GtkWidget *widget, GdkEvent *event, GtkData *data) {
 void create_ui(GtkData *data) {
   GtkWidget *main_window;
   GtkWidget *main_box;
+  GtkWidget *main_hbox, *controls;
+  GtkWidget *button;
 
   main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   g_signal_connect(G_OBJECT(main_window), "delete-event",
                    G_CALLBACK(delete_event_cb), data);
 
+  main_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+  gtk_box_pack_start(GTK_BOX(main_hbox), data->sink_widget, TRUE, TRUE, 0);
+
+  controls = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+  data->controls_widget = controls;
+
   main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-  gtk_box_pack_start(GTK_BOX(main_box), data->sink_widget, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(main_box), main_hbox, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(main_box), controls, FALSE, FALSE, 0);
 
   gtk_container_add(GTK_CONTAINER(main_window), main_box);
   gtk_window_set_default_size(GTK_WINDOW(main_window), 640, 480);
 
   gtk_widget_show_all(main_window);
+}
+
+static void on_button_click(GtkButton *button, GtkData *data) {
+  // reqeuest vedio from node
+  GError *error = NULL;
+
+  GSocket *socket = g_socket_new(G_SOCKET_FAMILY_IPV4, G_SOCKET_TYPE_DATAGRAM,
+                                 G_SOCKET_PROTOCOL_UDP, &error);
+
+  const char *dest_ip =
+      (const char *)g_object_get_data(G_OBJECT(button), "node-ip");
+
+  GSocketAddress *dest_addr =
+      g_inet_socket_address_new_from_string(dest_ip, 6000);
+
+  char message[128];
+  g_snprintf(message, sizeof(message), "%s", data->local_ip);
+
+  // Transmit the command string instantly over the mesh link
+  g_socket_send_to(socket, dest_addr, message, strlen(message), NULL, &error);
+  if (error != NULL) {
+    g_printerr("[Socket Error]: %s\n", error->message);
+    g_clear_error(&error);
+  }
+
+  g_object_unref(dest_addr);
+  g_object_unref(socket);
+  g_object_unref(error);
+}
+
+static void add_button(GtkData *data, char dest_ip[64]) {
+  GtkWidget *button;
+  button = gtk_button_new();
+
+  gtk_button_set_label(GTK_BUTTON(button), dest_ip);
+
+  g_object_set_data_full(G_OBJECT(button), "node-ip", g_strdup(dest_ip),
+                         g_free);
+
+  g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(on_button_click),
+                   data);
+
+  gtk_box_pack_start(GTK_BOX(data->controls_widget), button, FALSE, FALSE, 2);
+}
+
+gboolean refresh_ui(GtkData *data) {
+  int neighbor_count;
+  MeshNeighbor neighbors[neighbor_count];
+
+  neighbor_count = fetch_mesh_neighbors(neighbors);
+
+  for (int i = 0; i < neighbor_count; i++) {
+    add_button(data, neighbors[i].ip);
+  }
+  return TRUE;
 }
