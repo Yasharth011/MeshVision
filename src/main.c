32d9df0a572f5ea09b19e423b6b3c36e82af0384
Yasharth011 <yasharthgupta10@gmail.com>
2026-06-15 18:19:49 +0530

    main: reformatting

--- src/main.c
+++ src/main.c
@@ -221,11 +221,11 @@ int main(int argc, char *argv[]) {
 
   // get local ip addr
   if (!get_local_ip("bat0:avahi", local_ip)) {
-    g_printerr("Error: Could not get interface bat0:avahi");
+    g_printerr("Error: Could not get interface bat0:avahi\n");
   } else if(!get_local_ip("bat0", local_ip)) {
-    g_printerr("Error: Could not get interface bat0");
+    g_printerr("Error: Could not get interface bat0\n");
   } else {
-    g_printerr("Error: Could not get local ip");
+    g_printerr("Error: Could not get local ip\n");
     return -1;
   }
 
